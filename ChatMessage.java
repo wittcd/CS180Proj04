98377456ac8bcb88bb300511434b1b306e8934be
HI123123 <justinchen2012@gmail.com>
2018-11-25 16:50:25 -0500

    added header

--- ChatMessage.java
+++ ChatMessage.java
@@ -1,5 +1,11 @@
 import java.io.Serializable;
 
+/**
+ * ChatMessage class, represents a message sent by a user
+ * @author Colin Witt, Justin Chen
+ * @version 1.0.0
+ * 
+ */
 final class ChatMessage implements Serializable {
     private static final long serialVersionUID = 6898543889087L;
 
